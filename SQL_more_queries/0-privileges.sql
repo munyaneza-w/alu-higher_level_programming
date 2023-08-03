677550bd0583147b8b40f9825e83801f7296a435
munyaneza-w <w.munyaneza@alustudent.com>
2023-08-03 01:11:14 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- privileges!
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all priviledges of SQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,3 @@
--- cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	UNIQUE(id),
-	FOREIGN KEY(state_id) REFERENCES states(id)
-	);
+-- add to the  table
+INSERT INTO first_table (id, name)
+VALUES (89, 'Holberton School');
