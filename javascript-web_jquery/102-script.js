fe50c5160ec520cca7a7cdfc080fa5e1974958f3
munyaneza-w <w.munyaneza@alustudent.com>
2023-10-07 00:19:53 -0700

    102-script.js

---
+++ javascript-web_jquery/102-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    var languageCode = $('#language_code').val();
+
+    $.ajax({
+      url: 'https://www.fourtonfish.com/hellosalut/hello/',
+      data: { lang: languageCode },
+      success: function(response) {
+        $('#hello').text(response.hello);
+      },
+      error: function() {
+        $('#hello').text('Error occurred while fetching translation.');
+      }
+    });
+  });
+});
